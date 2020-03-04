8475f06f2f2ee2b192cb1cfd754f1e8e44b4c15f
ThomasDroege <ThomasDroege@gmx.de>
2020-03-04 21:50:37 +0100

    Bootstraped employees and projects tho the database via CommandLineRunner.

--- src/main/java/com/thomas/pma/entities/Project.java
+++ src/main/java/com/thomas/pma/entities/Project.java
@@ -1,5 +1,6 @@
 package com.thomas.pma.entities;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -80,6 +81,13 @@ public class Project {
 
 	public void setEmployees(List<Employee> employees) {
 		this.employees = employees;
+	}
+	
+	public void addEmployee(Employee emp) {
+		if(employees==null) {
+			employees = new ArrayList<>();
+		}
+		employees.add(emp);
 	}
 	
 	

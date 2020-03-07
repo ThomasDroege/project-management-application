406350418a255c851fcb7b390eb3f2e5030b536c
ThomasDroege <ThomasDroege@gmx.de>
2020-03-07 20:36:29 +0100

    Parsing application attributes to the controller -> model -> view.

--- src/main/java/com/thomas/pma/controllers/HomeController.java
+++ src/main/java/com/thomas/pma/controllers/HomeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,13 +17,12 @@ import com.thomas.pma.dao.ProjectRepository;
 import com.thomas.pma.dto.ChartData;
 import com.thomas.pma.dto.EmployeeProject;
 import com.thomas.pma.entities.Project;
-import com.thomas.pma.springExample.Car;
 
 @Controller
 public class HomeController {
 
-	@Autowired
-	Car car;
+	@Value("${version}")
+	private String ver;
 	
 	@Autowired
 	ProjectRepository proRepo;
@@ -32,6 +32,9 @@ public class HomeController {
 	
 	@GetMapping("/")
 	public String displayHome(Model model) throws JsonProcessingException {
+		
+		//store application.property to the model -> view
+		model.addAttribute("versionNumber", ver);
 		
 		Map<String, Object> map = new HashMap<>();
 		

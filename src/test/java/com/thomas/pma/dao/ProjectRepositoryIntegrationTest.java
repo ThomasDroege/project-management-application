6a1728850900d9df671139d25ec74fbe587bef5d
ThomasDroege <ThomasDroege@gmx.de>
2020-03-08 12:21:49 +0100

    Added @SpringBootTest and deleted the @ContextConfiguration Annotation by moving the testclass to com.thomas.pma package.

--- src/test/java/com/thomas/pma/dao/ProjectRepositoryIntegrationTest.java
+++ src/test/java/com/thomas/pma/dao/ProjectRepositoryIntegrationTest.java
@@ -1,24 +1,21 @@
-package com.thomas.dao;
+package com.thomas.pma.dao;
 
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.thomas.pma.ProjectManagementApplication;
 import com.thomas.pma.dao.ProjectRepository;
 import com.thomas.pma.entities.Project;
 
-@ContextConfiguration(classes=ProjectManagementApplication.class)
+@SpringBootTest
 @RunWith(SpringRunner.class)
-@DataJpaTest
 @SqlGroup({@Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD, scripts= {"classpath:schema.sql","classpath:data.sql"}),
 			@Sql(executionPhase = ExecutionPhase.AFTER_TEST_METHOD, scripts= "classpath:drop.sql")})
 public class ProjectRepositoryIntegrationTest {

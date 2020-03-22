0f9fb6860be157449d926eb5a2e6087938780609
ThomasDroege <ThomasDroege@gmx.de>
2020-03-22 13:44:25 +0100

    Added basic loging for controllers.

--- src/main/java/com/thomas/pma/logging/ApplicationLoggerAspect.java
+++ src/main/java/com/thomas/pma/logging/ApplicationLoggerAspect.java
@@ -1,6 +1,9 @@
 package com.thomas.pma.logging;
 
-import org.aspectj.lang.annotation.After;
+import java.util.Arrays;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
@@ -15,15 +18,35 @@ public class ApplicationLoggerAspect {
 	
 	//method points out where does the log code to be executed
 	//. = classes ..=classes, methods  ..*= classes, methods and everything else
-	@Pointcut("within(com.thomas.pma.controllers..*)"
-			+ "|| within(com.thomas.pma.dao..*)")
+	@Pointcut("within(com.thomas.pma.controllers..*)")
 	public void definePackagePointcuts() {
 		//empty method just to name the location specified in the pointcut
 	}
 	
 	//method with the code to be run at the pointcut
-	@After("definePackagePointcuts()")
-	public void log() {
-		log.debug(" ------------------------------------------------------ ");
+	@Around("definePackagePointcuts()")
+	public Object logAround(ProceedingJoinPoint jp) {
+		log.debug(" \n \n \n ");
+		log.debug("********** Before Method Execution ********** \n{}. {} () with arguments[s] = {}",
+				jp.getSignature().getDeclaringTypeName(),
+				jp.getSignature().getName(), Arrays.toString(jp.getArgs()));
+		log.debug("_____________________________________________\n \n \n");
+		
+		Object o = null;
+		
+		try {
+			o = jp.proceed();
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		
+		log.debug(" \n \n \n ");
+		log.debug("********** Before Method Execution ********** \n{}. {} () with arguments[s] = {}",
+				jp.getSignature().getDeclaringTypeName(),
+				jp.getSignature().getName(), Arrays.toString(jp.getArgs()));
+		log.debug("_____________________________________________\n \n \n");
+		
+		return o;
+		
 	}
 }

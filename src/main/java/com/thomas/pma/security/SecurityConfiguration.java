93629e5fc088d6fb705145a69319d21b77c49cc4
ThomasDroege <ThomasDroege@gmx.de>
2020-04-12 12:07:24 +0200

    fixed Authority in Security Configuration. Closes #12

--- src/main/java/com/thomas/pma/security/SecurityConfiguration.java
+++ src/main/java/com/thomas/pma/security/SecurityConfiguration.java
@@ -43,10 +43,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		// role = 'ADMIN' as SQL Statement
 		//.antMatchers("/projects/new").hasRole("ADMIN")
 		
-		.antMatchers("/projects/new").hasRole("ADMIN")
-		.antMatchers("/projects/save").hasRole("ADMIN")
-		.antMatchers("/employees/new").hasRole("ADMIN")
-		.antMatchers("/employees/save").hasRole("ADMIN")
+		
+		.antMatchers("/projects/new").hasAuthority("ADMIN")
+		.antMatchers("/projects/save").hasAuthority("ADMIN")
+		.antMatchers("/employees/new").hasAuthority("ADMIN")
+		.antMatchers("/employees/save").hasAuthority("ADMIN")
 		.antMatchers("/","/**").permitAll()
 		.and()
 		.formLogin();

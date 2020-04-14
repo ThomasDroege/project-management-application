0dca430d6f339ad8215a131b66cf3290cc14f0ee
ThomasDroege <ThomasDroege@gmx.de>
2020-04-14 21:50:09 +0200

    Added Pagination for Employee Rest Request.

--- src/main/java/com/thomas/pma/dao/EmployeeRepository.java
+++ src/main/java/com/thomas/pma/dao/EmployeeRepository.java
@@ -3,12 +3,12 @@ package com.thomas.pma.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.thomas.pma.dto.EmployeeProject;
 import com.thomas.pma.entities.Employee;
 
-public interface EmployeeRepository extends CrudRepository<Employee, Long>{
+public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long>{
 	
 	@Override
 	public List<Employee> findAll();
